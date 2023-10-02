6bd943962fc86073c2ee02fba34bedce51e6f3a7
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-10-02 23:09:40 +0530

    04. missing element in sorted array

---
+++ 05. Searching/Binary Search/Binary Search Problems/04_question.cpp
@@ -0,0 +1,52 @@
+// You are given a sorted array of N integers from 1 to N with one number missing find the missing number Expected time complexity O(logn) 
+
+// Examples: 
+ 
+
+// Input :ar[] = {1, 3, 4, 5}
+// Output : 2
+
+// Input : ar[] = {1, 2, 3, 4, 5, 7, 8}
+// Output : 6
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMissingElement(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+
+    while(start <= end){
+        int diff = arr[mid] - mid;
+
+        if(diff == 1){
+            // right me 
+            start = mid+1;
+        }
+        else{
+            // store ans 
+            ans = mid;
+            // left me jana 
+            end = mid - 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    if(ans+1 == 0)
+    return n+1;
+    return ans+1;
+}
+
+int main(){
+ 
+//  int arr[] = {1,2,3,4,6,7,8};
+int arr[] = {1,2,3,4,5,6,7,8};
+ int n = 8;
+
+ int missintNumber = findMissingElement(arr, n);
+ cout<<"missing number is: "<<missintNumber<<endl;
+
+ return 0;
+}
