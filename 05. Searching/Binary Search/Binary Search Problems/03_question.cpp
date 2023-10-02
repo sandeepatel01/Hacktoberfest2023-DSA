6524400877301f301de11a6a1313838c24dd2dd6
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-10-02 23:06:34 +0530

    03. Total occurrence of element in sorted array

---
+++ 05. Searching/Binary Search/Binary Search Problems/03_question.cpp
@@ -0,0 +1,85 @@
+// Given a sorted array arr[] and a number x, write a function that counts the occurrences of x in arr[]. Expected time complexity is O(Logn)
+
+// Examples:
+//   Input: arr[] = {1, 1, 2, 2, 2, 2, 3,},   x = 2
+//   Output: 4 // x (or 2) occurs 4 times in arr[]
+
+//   Input: arr[] = {1, 1, 2, 2, 2, 2, 3,},   x = 3
+//   Output: 1
+
+//   Input: arr[] = {1, 1, 2, 2, 2, 2, 3,},   x = 1
+//   Output: 2
+
+//   Input: arr[] = {1, 1, 2, 2, 2, 2, 3,},   x = 4
+//   Output: -1 // 4 doesn't occur in arr[]
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int findFirstOccurance(int arr[], int n, int target){
+    int start = 0; 
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+
+    while(start <= end){
+        if(target == arr[mid]){
+            ans = mid;    
+            end = mid - 1;
+        }
+        else if(target > arr[mid]){
+            start = mid + 1;
+        }
+        else if(target < arr[mid]){
+            end = mid-1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+
+int findLasttOccurance(int arr[], int n, int target){
+    int start = 0; 
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+
+    while(start <= end){
+        if(target == arr[mid]){
+            ans = mid;    
+            start = mid+1;
+        }
+        else if(target > arr[mid]){
+            start = mid + 1;
+        }
+        else if(target < arr[mid]){
+            end = mid-1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+
+int totalOccurance(int arr[], int n, int target){
+    int firstOcc = findFirstOccurance(arr, n, target);
+    int lastOcc = findLasttOccurance(arr, n, target);
+
+    int total = lastOcc - firstOcc + 1;
+    return total;       
+}
+
+int main(){
+ 
+ int arr[] = {10,20,30,40,50,60,70,80,90};
+ int n=9;
+ int target = 30;
+
+ int ans = findLasttOccurance(arr, n, target);
+ cout<<"total occurance is: "<<ans<<endl;
+
+ return 0;
+}
